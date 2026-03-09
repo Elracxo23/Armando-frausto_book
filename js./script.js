ba0b48fbfd4bc30637202f729827aa848e9bf465
Elracxo23 <elratzoo@gmail.com>
2026-03-09 00:57:37 -0600

    Update script.js

--- js./script.js
+++ js./script.js
@@ -1,12 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-const menuBtn = document.querySelector(".menu-hamburguesa");
-const sideMenu = document.querySelector(".side-menu");
-
-menuBtn.addEventListener("click", function () {
-
-sideMenu.classList.toggle("active");
-
-});
+alert("script funcionando");
 
 });
