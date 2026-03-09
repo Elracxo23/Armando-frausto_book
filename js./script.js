af6e90f6c4bf82a2081e06faa227c917aa03e35a
Elracxo23 <elratzoo@gmail.com>
2026-03-09 00:50:15 -0600

    Update script.js

--- js./script.js
+++ js./script.js
@@ -1,19 +1,8 @@
 const menuBtn = document.querySelector(".menu-hamburguesa");
 const sideMenu = document.querySelector(".side-menu");
-const menuLinks = document.querySelectorAll(".side-menu a");
 
-menuBtn.addEventListener("click", () => {
+menuBtn.addEventListener("click", function() {
 
 sideMenu.classList.toggle("active");
-
-});
-
-menuLinks.forEach(link => {
-
-link.addEventListener("click", () => {
-
-sideMenu.classList.remove("active");
-
-});
 
 });
